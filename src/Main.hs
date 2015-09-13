e25b55bed46c7f02d328d0e5db9d3a126bcd2626
Hector Orellana <pulga.p.29@gmail.com>
2015-09-13 20:40:08 -0300

    function to count lines from stdin

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,18 @@
 module Main where
 
 import Data.Conduit
-import qualified Data.Conduit.List as CL
-import qualified Data.Conduit.Binary as CB
   
+import qualified Data.Conduit.Text as CT
+import qualified Data.Conduit.Combinators as CB
+
+import Control.Monad.Trans.Resource
+import System.IO (stdin)
+
+countLines :: IO Int
+countLines = runResourceT $
+                  CB.sourceHandle stdin $=
+                  CT.lines $$
+                  CB.foldl (\c _ -> c + 1) 0
+             
 main :: IO ()
-main = undefined
-  
-  
+main = countLines >>= print
