966bc79e2c42501e75fbf0f66d5edd8f7d41216b
Hector Orellana <pulga.p.29@gmail.com>
2015-09-13 20:48:35 -0300

    function to count lines in a file

--- src/Main.hs
+++ src/Main.hs
@@ -8,11 +8,10 @@ import qualified Data.Conduit.Combinators as CB
 import Control.Monad.Trans.Resource
 import System.IO (stdin)
 
-countLines :: IO Int
-countLines = runResourceT $
-                  CB.sourceHandle stdin $=
+countLines :: (Num b, MonadResource m) => FilePath -> m b
+countLines file = CB.sourceFile file $=
                   CT.lines $$
                   CB.foldl (\c _ -> c + 1) 0
-             
+
 main :: IO ()
-main = countLines >>= print
+main = undefined
