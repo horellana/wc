a693b66747428edea98ed91ed348210343d2209e
Hector Orellana <pulga.p.29@gmail.com>
2015-09-13 21:58:00 -0300

    get files as argument and output total lines

--- src/Main.hs
+++ src/Main.hs
@@ -5,13 +5,19 @@ import Data.Conduit
 import qualified Data.Conduit.Text as CT
 import qualified Data.Conduit.Combinators as CB
 
+import Control.Monad
 import Control.Monad.Trans.Resource
+
+import System.Environment (getArgs)
 import System.IO (stdin)
 
-countLines :: (Num b, MonadResource m) => FilePath -> m b
+countLines :: (MonadResource m) => FilePath -> m Int
 countLines file = CB.sourceFile file $=
                   CT.lines $$
                   CB.foldl (\c _ -> c + 1) 0
 
 main :: IO ()
-main = undefined
+main = do
+  files <- getArgs
+  fileLines <- runResourceT $ forM files countLines
+  print $ sum fileLines
