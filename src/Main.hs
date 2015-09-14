01482efc1ba0aad5ce00bafd9c328e501319a36e
Hector Orellana <pulga.p.29@gmail.com>
2015-09-14 03:15:00 -0300

    parse command line args, handle multiple files.

--- src/Main.hs
+++ src/Main.hs
@@ -1,23 +1,39 @@
 module Main where
-
+    
 import Data.Conduit
+
+import Options.Applicative
   
 import qualified Data.Conduit.Text as CT
 import qualified Data.Conduit.Combinators as CB
 
 import Control.Monad
 import Control.Monad.Trans.Resource
+    
+import Data.List (intercalate)
+    
+data CmdArguments = CmdArguments { optLines :: Bool, 
+                                   optFiles :: [FilePath] }
+                  deriving (Show)
 
-import System.Environment (getArgs)
-import System.IO (stdin)
-
+cmdArguments :: Parser CmdArguments
+cmdArguments = CmdArguments 
+               <$> switch (long "lines"
+                          <> help "Count lines in FILES")
+               <*> many (argument str (metavar "FILES"))
+                   
 countLines :: (MonadResource m) => FilePath -> m Int
-countLines file = CB.sourceFile file $=
-                  CT.lines $$
-                  CB.foldl (\c _ -> c + 1) 0
-
+countLines file = CB.sourceFile file $= CT.lines $$ CB.length
+        
+wc :: CmdArguments -> IO String
+wc (CmdArguments True files) = do
+  results <- runResourceT $ forM files $ 
+       \file -> do count <- countLines file
+                   return $ show count ++ " " ++ file
+  return $ intercalate "\n" results
+                          
 main :: IO ()
-main = do
-  files <- getArgs
-  fileLines <- runResourceT $ forM files countLines
-  print $ sum fileLines
+main = execParser parserInfo >>= wc >>= putStrLn
+    where
+      parserInfo = info (helper <*> cmdArguments) fullDesc
+                   
